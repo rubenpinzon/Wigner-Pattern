fd62c44c4a9d25eb89ddf2020c66d767c7cbe3e6
ruben <ruben.pinzon@wigner.mta.hu>
2015-06-22 14:09:08 +0200

    fastFa implemented

--- corr.py
+++ corr.py
@@ -12,8 +12,8 @@ parser.add_argument("-p", '--PATH', type=str, nargs='*',
 parser.add_argument("-w", "--window", type=float, nargs=1,
                     help="length of the correlation window. Default 10 ms",
                     default=0.01)
-parser.add_argument("-n", "--neurons", type=tuple, nargs=1,
-                    help="Tuple indicating the neurons to correlate. Default empty: i.e., autocorrelogram",
+parser.add_argument("-n", "--neurons", type=int, nargs=1,
+                    help="Neurons to compute. Default empty: i.e., autocorrelogram",
                     default=tuple())
 
 color_ascii = {'green': '\033[1;32m{t}\033[1;m', 'red': '\033[1;31m{t}\033[1;m'}
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     data = sio.loadmat(args.PATH[0])
     window = args.window[0]
+    #neu_num = args.neurons[0]
     events = data['Spike'][0]['res'][0]
     clusters = data['Spike'][0]['totclu'][0]
     fs = 1250.0
@@ -30,20 +31,25 @@ if __name__ == '__main__':
     #  extract the spikes times for each neuron
     fig = plt.figure(frameon=False, figsize=(9, 4), dpi=80, facecolor='w', edgecolor='k')
     ax1 = fig.add_axes([0, 0.2, 1, 0.8])
-    ax2 = fig.add_axes([0, 0, 1, 0.2])
+    ax2 = fig.add_axes([0, 0, 1, 0.1])
     ax1.axis('off')
     ax2.axis('off')
 
     spikes = {}
     for neuron in range(1, max(clusters)):
         spikes['neuron {}'.format(neuron)] = events[clusters == neuron] / fs
-    # max time based on the time of the last spike
 
+    # max time based on the time of the last spike
+    fig = plt.figure(frameon=False, figsize=(9, 4), dpi=80, facecolor='w', edgecolor='k')
+    ax1 = fig.add_axes([0, 0.2, 1, 0.8])
+    ax1.axis('off')
     corr = np.zeros((max(clusters)-1, int(2*np.ceil(window/dt))))
     for n in range(len(spikes)):
         corr[n, :] = brian.statistics.autocorrelogram(T0=spikes['neuron {}'.format(n+1)], width=window, bin=dt)
+        #  ax1.bar(left=range(int(2*np.ceil(window/dt))), height=corr[n, :])
+        ax1.plot(corr[n, :])
 
-    ax1.bar(left=range(int(2*np.ceil(window/dt))), height=corr[88, :])
-    plt.ylim((0, 1))
-    ax2.plot(spikes['neuron 88'], np.ones(np.shape(spikes['neuron 88'])), linestyle='None', marker='|')
+    np.savetxt('autocorr1000ms.txt',corr, fmt='%3.4f\t')
+
+    plt.ylim((0, 0.1))
     plt.show()
--- processHC5.py
+++ processHC5.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description='Function to plot a matlab processe
 parser.add_argument('PATH', type=str, nargs='+',
                     help='Path to the mat file ##.BehavElectrData.mat')
 parser.add_argument("--verbosity", help="increase output verbosity")
-color_ascii = {'green': '\033[1;32m{t}\033[1;m', 'red': '\033[1;31m{t}\033[1;m'}
+color_ascii = {'green': '\033[1;32m{t}\033[1;m', 'red': '\033[1;31m{t}\033[1;m', 'blue': '\033[1;34m{t}\033[1;m'}
 
 #  Manage mouse events in the raster plot and print neurons name
 class LabelPrinter:
@@ -33,14 +33,15 @@ def get_psth(data, bin_size, **kwargs):
 
     if len(keys) != 0:
         bin_min, bin_max = (0, np.ceil(max(data[max(data)])))
-        psth = np.zeros(np.ceil(bin_max/bin_size), dtype=int)
+        psth = np.zeros(np.ceil(bin_max / bin_size), dtype=int)
         for k in keys:
             spike_bin = data[k]
-            hist, _ = np.histogram(a=spike_bin, bins=np.ceil(bin_max/bin_size), range=(bin_min, bin_max))
+            hist, _ = np.histogram(a=spike_bin, bins=np.ceil(bin_max / bin_size), range=(bin_min, bin_max))
             psth += hist
-        return psth*1000./(len(keys)*bin_size)
+        return psth * 1000. / (len(keys) * bin_size)
     else:
         return None
+
 
 def get_scalogram(data, **kwargs):
     if 'wavelet' in kwargs:
@@ -53,19 +54,21 @@ def get_scalogram(data, **kwargs):
         levels = np.arange(1, 11)
     return signal.cwt(data, wavelet, levels)
 
+
 # First-order statistics
 def firing_rate(spikes):
     '''
     Rate of the spike train.
     '''
-    return (len(spikes)-1)/(spikes[-1]-spikes[0])
+    return (len(spikes) - 1) / (spikes[-1] - spikes[0])
+
 
 def CV(spikes):
     '''
     Coefficient of variation.
     '''
-    ISI = np.diff(spikes) # interspike intervals
-    return np.std(ISI)/np.mean(ISI)
+    ISI = np.diff(spikes)  # interspike intervals
+    return np.std(ISI) / np.mean(ISI)
 
 
 if __name__ == '__main__':
@@ -96,7 +99,14 @@ if __name__ == '__main__':
         spikes['neuron {}'.format(neuron)] = events[clusters == neuron] / fs
         average_firing.append(firing_rate(spikes['neuron {}'.format(neuron)]))
         coeff_var.append(CV(spikes['neuron {}'.format(neuron)]))
-        print 'Firing rate of neuron {} = {:03.2f} Hz, and coeff. variation {:03.2f}'.format(neuron,average_firing[-1], coeff_var[-1])
+        if neuron < 32:
+            c = 'green'
+        elif neuron < 64:
+            c = 'red'
+        else:
+            c = 'blue'
+        print 'Firing rate of neuron {} = {:03.2f} Hz, and coeff. variation {:03.2f}'.format(
+            color_ascii[c].format(t=neuron), average_firing[-1], coeff_var[-1])
 
     # distribution of firing rates
     time_max = max(spikes[max(spikes)])
@@ -104,19 +114,19 @@ if __name__ == '__main__':
     n, bins = np.histogram(average_firing, bins=10)
     his_fr = plt.figure(frameon=False, figsize=(9, 7), dpi=80, facecolor='w', edgecolor='k')
     his_ax = his_fr.add_axes([0.1, 0.1, 0.8, 0.8])
-    pos = np.arange(len(n))+0.5
+    pos = np.arange(len(n)) + 0.5
     his_ax.barh(pos, n, align='center', height=.5, color='m')
     plt.title('Firing rate distribution')
-    plt.yticks(pos-.25, ['{:03.2f} Hz'.format(b) for b in bins])
+    plt.yticks(pos - .25, ['{:03.2f} Hz'.format(b) for b in bins])
 
     # distribution of ISI
     n, bins = np.histogram(coeff_var, bins=10)
     his_cv = plt.figure(frameon=False, figsize=(9, 7), dpi=80, facecolor='w', edgecolor='k')
     his_cv_ax = his_cv.add_axes([0.1, 0.1, 0.8, 0.8])
-    pos = np.arange(len(n))+0.5
+    pos = np.arange(len(n)) + 0.5
     his_cv_ax.barh(pos, n, align='center', height=.5, color='m')
     plt.title('Distribution of CV')
-    plt.yticks(pos-.25, ['{:03.2f}'.format(b) for b in bins])
+    plt.yticks(pos - .25, ['{:03.2f}'.format(b) for b in bins])
 
     #  Configure the figure's window
     fig = plt.figure(frameon=False, figsize=(17, 10), dpi=80, facecolor='w', edgecolor='k')
@@ -133,12 +143,12 @@ if __name__ == '__main__':
     psth = get_psth(data=spikes, bin_size=.05)
     ax2 = fig.add_axes([0, 0.2, 1, 0.1], sharex=ax)
     ax2.axis('off')
-    ax2.plot(np.arange(start=0, stop=time_max, step=time_max/len(psth)), psth)
+    ax2.plot(np.arange(start=0, stop=time_max, step=time_max / len(psth)), psth)
 
     ax3 = fig.add_axes([0, 0, 1, 0.2], sharex=ax)
     ax3.axis('off')
-    ax3.plot(np.arange(start=0, stop=time_max, step=time_max/len(eeg)), eeg, color='r')
-    ax3.plot(np.arange(start=0, stop=time_max, step=time_max/len(mazeId)), (mazeId*2000)-10000., color='k')
+    ax3.plot(np.arange(start=0, stop=time_max, step=time_max / len(eeg)), eeg, color='r')
+    ax3.plot(np.arange(start=0, stop=time_max, step=time_max / len(mazeId)), (mazeId * 2000) - 10000., color='k')
 
     canvas, = ax.plot([0], [0])  # empty line to get the axis
     LabelPrinter = LabelPrinter(spikes.keys(), canvas)

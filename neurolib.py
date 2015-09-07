0aaec255350e196606e9b8f6998d5e83437819e2
ruben <ruben.pinzon@wigner.mta.hu>
2015-09-07 09:01:44 +0200

    working on spatial bining and decoding

--- neurolib.py
+++ neurolib.py
@@ -364,12 +364,15 @@ def get_cells(path, section=None, only_pyr=None, verbose=False):
     :param section: Name of the section to extract (default: None)
                     Run, Wheel, or Other
     :param only_pyr: return only pyramidal cells
-    :return neuron: list with the spikes for each cell and lap is section
-                    provided
-    :return space: X, Y coordinates of animal in the given section
+    :return neuron: (N, 2, Laps), where the second dimension of the list
+                    contains the x-y coordinates of the N-spike
+    :return trajectory_dict: dict with X, Y coordinates of animal in the running section
+                    with 'left' and 'right' keys.
 
     """
     # TODO: implement section wheel and other sections if needed
+    from scipy.ndimage import filters as fil
+
     data = sio.loadmat(path)
     clusters = np.squeeze(data['Spike']['totclu'][0, 0])
     spikes = np.squeeze(data['Spike']['res'][0, 0])
@@ -377,15 +380,24 @@ def get_cells(path, section=None, only_pyr=None, verbose=False):
     sections = np.squeeze(data['Par']['MazeSectEnterLeft'][0, 0])
     X = np.squeeze(data['Track']['X'][0, 0])
     Y = np.squeeze(data['Track']['Y'][0, 0])
+    x_spk = np.squeeze(data['Spike']['X'][0, 0])
+    y_spk = np.squeeze(data['Spike']['Y'][0, 0])
+    hit = np.squeeze(data['Par']['BehavType'][0, 0]) == 1
     # Separate spikes by neuron number
-    neuron = list()
-    space = list()
+    neuron_xy = list()
+    neuron_spk = list()
+    trajectory_left = list()
+    trajectory_right = list()
+    num_laps = len(sections)
+
     for n in range(1, max(clusters) + 1):
 
         if only_pyr and isIntern[n - 1]:
             continue
         spk = spikes[clusters == n]
-        if verbose: print 'neuron {}-th with {} spks'.format(n, len(spk))
+        xy_cell = [x_spk[clusters == n], y_spk[clusters == n]]
+        if verbose:
+            print 'neuron {}-th with {} spks'.format(n, len(spk))
 
         if section == 'Run':
             # Get intervals of interest: Section 2 to 6 that correspond to the running sections.
@@ -394,21 +406,53 @@ def get_cells(path, section=None, only_pyr=None, verbose=False):
             if verbose:
                 print 'neuron {}-th is pyramidal with {} spks'.format(n, len(spk))
 
-            num_laps = len(sections)
             laps = list()
+            xy_laps = list()
             for i in range(num_laps):
                 start_run, end_run = sections[i][1, 0], sum(sections[i][4:6, 1])
                 idx = np.where(np.logical_and(spk >= start_run, spk <= end_run))
                 # save spike events aligned to the entering to sect 2.
                 laps.append(spk[idx] - start_run)
-                # Extract the animal space coordinates
-                space.append((X[start_run:end_run], Y[start_run:end_run]))
-            neuron.append(laps)
+                xy_laps.append((xy_cell[0][idx], xy_cell[1][idx]))
+
+            neuron_spk.append(laps)
+            neuron_xy.append(xy_laps)
         else:
-            neuron.append(spk)
-    print '{} cells extracted'.format(len(neuron))
+            neuron_spk.append(spk)
+            neuron_xy.append(xy_cell)
+
+    neuron = {'spikes': neuron_spk, 'xy': neuron_xy}
+    # extract position per lap
+    for i in range(num_laps):
+        if hit[i]:
+            start_run, end_run = sections[i][1, 0], sum(sections[i][4:6, 1])
+            # split the trajectories based on the Y position
+            if Y[start_run:end_run].mean() > 500:
+                trajectory_left.append((X[start_run:end_run], Y[start_run:end_run]))
+            else:
+                trajectory_right.append((X[start_run:end_run], Y[start_run:end_run]))
+
+    trajectory_dict = {'left': trajectory_left, 'right': trajectory_right}
+    # compute the average trajectories by interpolating to the same length all
+    #  the trajectories in the same direction
+    trajectory_dict_interp = dict()
+    for dir, trajectory in trajectory_dict.iteritems():
+        _, max_time = np.shape(max(trajectory, key=lambda x: np.shape(x)[1]))
+        trajectory_inter = list()
+        for tra in trajectory:
+            x, y = tra[0], tra[1]
+            xp = np.interp(range(max_time), range(len(x)), x)
+            yp = np.interp(range(max_time), range(len(y)), y)
+            trajectory_inter.append([xp, yp])
+        trajectory_dict_interp[dir + '_interp'] = trajectory_inter
+        trajectory_median = np.median(trajectory_inter, axis=0)
+        trajectory_dict_interp[dir + '_median'] = (
+            fil.gaussian_filter1d(trajectory_median[0], 50.), fil.gaussian_filter1d(trajectory_median[1], 50.))
+
+    trajectory_dict.update(trajectory_dict_interp)
+    print '{} cells extracted'.format(len(neuron['spikes']))
     print 'Loading completed'
-    return neuron, space
+    return neuron, trajectory_dict
 
 
 def raster(cells, title=''):
@@ -431,7 +475,7 @@ def raster(cells, title=''):
     plt.title(title)
 
 
-def plot_position(space, title=''):
+def plot_position(space, title='', **kwargs):
     """
 
     :param space: list of X,Y coordinates per lap
@@ -442,7 +486,7 @@ def plot_position(space, title=''):
     fig = plt.figure(frameon=False, figsize=(9, 7), dpi=80, facecolor='w', edgecolor='k')
     ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
     for position in space:
-        plt.plot(position[0], position[1])
+        plt.plot(position[0], position[1], **kwargs)
     plt.xlabel('Position (mm)')
     plt.ylabel('Position (mm)')
     plt.title(title)
@@ -490,3 +534,95 @@ def binned(train, bin_size=0.1):
             bin_start, bin_end = iter_bin * bin_width, (iter_bin + 1) * bin_width - 1
             y[:, iter_bin, lap] = np.sum(train[:, bin_start:bin_end, lap], axis=1)
     return y
+
+
+def rect_roi(centre, size, angle):
+    ROI = ([size[0] / 2, size[1] / 2],
+           [size[0] / 2, -size[1] / 2],
+           [-size[0] / 2, -size[1] / 2],
+           [-size[0] / 2, size[1] / 2],
+           [size[0] / 2, size[1] / 2])
+    rotation = ([np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)])
+    return np.dot(ROI, rotation) + centre
+
+
+def point_in_poly(x, y, poly):
+    """ Determine if a point is inside a given polygon or not
+        Polygon is a list of (x,y) pairs. This function
+        returns True or False.  The algorithm is called
+        the "Ray Casting Method.
+
+    :param x: list
+    :param y: list
+    :param poly: list of (x,y) pairs
+    :return: true false
+    """
+    n = len(poly)
+    inside = False
+
+    p1x, p1y = poly[0]
+    for i in range(n + 1):
+        p2x, p2y = poly[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
+
+def count_spikes(cells, arena_shape, grid_shape, verbose=-1):
+    """counts the spike inside the given grid
+
+    """
+    if verbose != -1:
+        import matplotlib.pyplot as plt
+        print 'verbose activated, showing cell {}'.format(verbose)
+        fig = plt.figure(frameon=False, figsize=(9, 7), dpi=80, facecolor='w', edgecolor='k')
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    num_bins = arena_shape / grid_shape
+    rois = list()
+    centers = list()
+    for x in range(0, arena_shape[0], grid_shape[0]):
+        for y in range(0, arena_shape[1], grid_shape[1]):
+            roi = rect_roi([x, y], grid_shape, 0).T
+            rois.append(roi)
+            centers.append((x, y))
+            if verbose != -1:
+                plt.plot(roi[0], roi[1], color=[0.5, 0.9, 0.5])
+
+    spike_count = list()
+    cell_idx = 0
+    for c in cells:
+        cx = [x for xy in c for x in xy[0]]
+        cy = [y for xy in c for y in xy[1]]
+        if verbose != -1 and cell_idx == verbose:
+            plt.plot(cx, cy, 'x', color='b')
+
+        count = list()
+        img = np.zeros(num_bins)
+        for poly, center in zip(rois, centers):
+            inside = 0
+            for x, y in zip(cx, cy):
+                if point_in_poly(x, y, poly.T):
+                    inside += 1
+            count.append(inside)
+            idx = center / grid_shape
+            img[idx[0], idx[1]] = inside
+            if verbose != -1 and cell_idx == verbose:
+                plt.text(center[0], center[1], '{}'.format(inside))
+                plt.title('Cell {}'.format(cell_idx))
+
+        if verbose != -1:
+            print 'Spike counted for Cell {}'.format(cell_idx)
+        spike_count.append(img)
+        cell_idx += 1
+
+    return spike_count
+
+
